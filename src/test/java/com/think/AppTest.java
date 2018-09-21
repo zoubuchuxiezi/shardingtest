8bda9815d85c735ddae7fb62206f9c994a24a61a
xieqihong <xieqihong@daojia-inc.com>
2018-09-21 19:23:01 +0800

    aaa

---
+++ src/test/java/com/think/AppTest.java
@@ -0,0 +1,34 @@
+package com.think;
+
+import com.think.main.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.think.dao.OrderMapper;
+import com.think.entity.TOrder;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AppTest {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Test
+    public void test() {
+        TOrder order = new TOrder();
+        order.setUserId(888);
+        order.setOrderId(77);
+        orderMapper.insert(order);
+    }
+
+    @Test
+    public void test2() {
+        TOrder order = orderMapper.findById(31);
+        System.out.println(order.getOrderId() + "" + order.getUserId());
+    }
+
+}
